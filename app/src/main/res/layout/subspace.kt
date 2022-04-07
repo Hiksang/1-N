976e225c63ead9ebf5264fa2616442130123bfc2
songingyu5252 <102507969+songingyu5252@users.noreply.github.com>
2022-04-07 17:21:01 +0900

    코틀린파일추가

---
+++ app/src/main/res/layout/subspace.kt
@@ -0,0 +1,14 @@
+package layout
+
+import ...
+
+
+class subspace : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.subspace)
+
+    }
+
+}
