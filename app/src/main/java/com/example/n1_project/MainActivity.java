c6f04081c2f8ad0d5a16d25332e113e98f2cfa58
Hiksang <goo9801@gmail.com>
2022-05-05 17:46:35 +0900

    로그인화면구현

---
+++ app/src/main/java/com/example/n1_project/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.n1_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn1,btn2,btn3,btn4;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn1 = (Button)findViewById(R.id.btn_1);
+        btn2 = (Button)findViewById(R.id.btn_2);
+        btn3 = (Button)findViewById(R.id.btn_3);
+        btn4 = (Button)findViewById(R.id.btn_4);
+
+        if(FirebaseAuth.getInstance().getCurrentUser() == null){
+            startSignUpActivity();
+        }
+        findViewById(R.id.logoutButton).setOnClickListener(onClickListener);
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()){
+                case R.id.logoutButton:
+                    FirebaseAuth.getInstance().signOut();
+                    startSignUpActivity();
+                    break;
+            }
+        }
+    };
+
+    private void startSignUpActivity() {
+        Intent intent = new Intent(this,SignUpActivity.class);
+        startActivity(intent);
+    }
+}
