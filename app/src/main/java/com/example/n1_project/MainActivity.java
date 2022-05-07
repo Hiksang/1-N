bfcdb37d74f4a0978efa5287b90f0c081d58f6bb
Hiksang <goo9801@gmail.com>
2022-05-08 00:58:01 +0900

    team make로 이동

--- app/src/main/java/com/example/n1_project/MainActivity.java
+++ app/src/main/java/com/example/n1_project/MainActivity.java
@@ -1,12 +1,11 @@
 package com.example.n1_project;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -27,10 +26,6 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-
-
-
 
 
 
