7ac44e1f25e5b076188079a8a936ef53b6a4d158
songingyu5252 <102507969+songingyu5252@users.noreply.github.com>
2022-05-21 12:22:07 +0900

    fff
    fff

---
+++ 1-N-main/app/src/main/java/com/example/n1_project/User.java
@@ -0,0 +1,49 @@
+package com.example.n1_project;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+public class  User {
+    private String location;
+    private String rn;
+    private String time;
+    private String uid;
+
+
+
+    public User(){}
+
+    public String getRn(){return rn;}
+
+    public void setRn(String rn) {
+        this.rn = rn;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getUid() { return uid;}
+
+    public void setUid(String uid) { this.uid = uid;}
+    //값을 추가할 때 쓰는 함수, MainActivity에서 addanimal함수에서 사용할 것임.
+    public User(String location, String rn, String time,String uid){
+        this.time = time;
+        this.location = location;
+        this.rn = rn;
+        this.uid = uid;
+    }
+
+
+}
