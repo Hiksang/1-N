2c25a12b0a510d3e05724646e1594e81c946a26d
5ivestarK <102516238+5ivestarK@users.noreply.github.com>
2022-05-21 15:22:59 +0900

    해보자
    파이팅

--- app/src/main/java/com/example/n1_project/option.java
+++ app/src/main/java/com/example/n1_project/option.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -39,6 +40,8 @@ public class option extends AppCompatActivity {
         Spinner spinner1 = (Spinner)findViewById(R.id.spinner);
         Spinner spinner2= (Spinner)findViewById(R.id.spinner_2);
         Button addBtn = findViewById(R.id.add_btn);
+        String uid;
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -77,15 +80,15 @@ public class option extends AppCompatActivity {
             public void onClick(View view) {
 
                 //입력값 변수 담기
-                addUser(tv_result2.getText().toString(),tv_result3.getText().toString(), tv_result.getText().toString());
+                addUser(tv_result2.getText().toString(),tv_result3.getText().toString(), tv_result.getText().toString(),uid);
 
             }
         });
     }
 
-    public void addUser(String rn,String time, String location) {
-        User user = new User(rn,time,location);
-        databaseReference.child("User").child("User_04").setValue(user);
+    public void addUser(String rn,String time, String location, String uid) {
+        User user = new User(rn,time,location,uid);
+        databaseReference.child("User").child(uid).push().setValue(user);
     }
     public void goNativeActivity(View v) {
         Intent intent = new Intent(getApplicationContext(), NativeActivity.class);
