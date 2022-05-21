06d8b796488adc8bbe62c6409fe20c2881c66469
5ivestarK <102516238+5ivestarK@users.noreply.github.com>
2022-05-21 22:29:04 +0900

    메뉴선택 삭제
    aaaa

--- app/src/main/java/com/example/n1_project/option.java
+++ app/src/main/java/com/example/n1_project/option.java
@@ -115,7 +115,7 @@ public class option extends AppCompatActivity {
     public void addUser(String rn,String time, String location, String uid, String menu) {
         User user = new User(rn,time,location,uid,menu);
 
-        databaseReference.child("Team").push().child(uid).setValue(user);
+        databaseReference.child("User").push().child(uid).setValue(user);
     }
     public void goNativeActivity(View v) {
         Intent intent = new Intent(getApplicationContext(), NativeActivity.class);
